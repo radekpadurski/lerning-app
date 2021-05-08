a43d38be25aecc1aac6dc7b1a62c7da7b21e8d6e
rpadurski <r.padurski@tkhtechnology.com>
2021-05-08 14:29:13 +0200

    Adding Button + Adding theme - styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import Button, { VARIANT } from "./components/button/Button";
+import defaultTheme from "./theme";
 
 function App() {
+  // const [theme, setTheme] = React.useState(defaultTheme);
+  // const updateTheme = () => {
+  //   setTheme({
+  //     primary: {
+  //       main: "#726a95",
+  //       contrastText: "#ffffff",
+  //     },
+  //     secondary: {
+  //       main: "#709fb0",
+  //       contrastText: "#ffffff",
+  //     },
+  //   });
+  // };
   return (
-    <h1>
-      Hello World
-    </h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant={VARIANT.PRIMARY}>PRIMARY BUTTON</Button>
+      <Button variant={VARIANT.SECONDARY}>SECONDARY BUTTON</Button>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme = {
+  palette: {
+    primary: {
+      main: "#726a95",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#709fb0",
+      contrastText: "#ffffff",
+    },
+  },
+};
+
+export default defaultTheme;
